7901ebcd354c9405485702f382327190a8196ebc
JC <jcsacra4@gmail.com>
2024-01-07 21:57:14 +0800

    delete logs

--- src/app/ui/listHeader.tsx
+++ src/app/ui/listHeader.tsx
@@ -7,9 +7,6 @@ type Props = {
 
 const ListHeader = (props: Props) => {
   
-    const signOut = () => {
-        console.log("sign out")
-    }
     return (
     <header className="h-20 px-10 flex items-center justify-between">
       <h1>{props.listName}</h1>
