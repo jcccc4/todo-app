b616bafbb261809f81b476271cdf5c6ca9e41642
JC <jcsacra4@gmail.com>
2024-01-09 14:23:56 +0800

    Learning server actions

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/app/lib/prisma";
+
 import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
 export async function GET(request: Request) {
   const users = await prisma.user.findMany();
@@ -15,5 +17,7 @@ export async function POST(request: Request) {
       authorId: authorId,
     },
   });
+
+  revalidatePath("/");
   return NextResponse.json({ newPost }, { status: 200 });
 }
--- src/app/ui/taskList.tsx
+++ src/app/ui/taskList.tsx
@@ -4,17 +4,11 @@ export interface Y {
   id: number;
   content: string;
 }
-
-function TaskList() {
-  const [taskList, setTaskList] = useState<Array<Y>>([]);
-  useEffect(() => {
-    fetch("/api/users/1/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setTaskList(data);
-      });
-  }, []);
+type Props = {
+  taskList: Array<Y>;
+}
+function TaskList({taskList}: Props) {
+ 
 
   return (
     <ul className="max-w-sm mx-auto ">
