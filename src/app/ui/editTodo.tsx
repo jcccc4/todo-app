bf9b5c9218570060aba85fd7562c987a70cdc449
JC <jcsacra4@gmail.com>
2024-01-16 16:40:25 +0800

    fix unused hooks

--- src/app/ui/editTodo.tsx
+++ src/app/ui/editTodo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useRef } from "react";
 import { editTodo } from "../actions/todoActions";
 
 type Props = {
