5d9795d68e1ab1a179a3b4bf23630b2c737ddd6a
JC <jcsacra4@gmail.com>
2024-02-09 13:21:23 +0800

    change case

--- src/app/_providers/QueryProviders.jsx
+++ src/app/_providers/QueryProviders.jsx
@@ -1,8 +1,6 @@
-// In Next.js, this file would be called: app/providers.jsx
+
 "use client";
 
-// We can not useState or useRef in a server component, which is why we are
-// extracting this part out into it's own file with 'use client' on top
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -14,7 +12,7 @@ export default function Providers({ children }) {
           queries: {
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
+            staleTime: 1000,
           },
         },
       })
