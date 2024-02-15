62b0d44691aaf52b083cf751d08b0d4fc2884104
JC <jcsacra4@gmail.com>
2024-02-15 18:49:56 +0800

    fix unused errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
-import { useRouter } from "next/navigation";
 
 export default async function Home() {
-  const router = useRouter();
   const session = await getServerSession(authOptions);
 
   console.log(session);
