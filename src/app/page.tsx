9691de2654d4437c9f1ef1a36f5830137520ffeb
JC <jcsacra4@gmail.com>
2024-02-15 18:46:41 +0800

    redirect pages on session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { useRouter } from "next/navigation";
+
 export default async function Home() {
+  const router = useRouter();
+  const session = await getServerSession(authOptions);
+  if (session) {
+    router.push("/dashboard");
+  }
+  console.log(session);
   return <main>Welcome this is your Home page.</main>;
 }
